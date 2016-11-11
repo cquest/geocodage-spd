ca21cecd6f0bff946ba541d9e937d9626cb9342c
cquest <cquest@openstreetmap.fr>
2016-11-11 19:55:53 +0100

    on conserve un log du traitement

--- 1_batch_dep.sh
+++ 1_batch_dep.sh
@@ -4,4 +4,4 @@ mkdir -p sirene
 mkdir -p sirene_geo
 
 echo {01..19} 2A 2B {21..99} | sed 's/ /\n/g' | \
-  parallel -j 24 -t csvgrep sirene_mini.csv -c DEPET -m {} \> sirene_{}.csv \; ./1b_sirene_geo.py sirene_{}.csv
+  parallel -j 24 -t csvgrep sirene_mini.csv -c DEPET -m {} \> sirene_{}.csv \; ./1b_sirene_geo.py sirene_{}.csv \> sirene_{}.csv.log
