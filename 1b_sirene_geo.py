3893423bf7f4773c4b167ba81f94bbaddda063cc
cquest <cquest@openstreetmap.fr>
2016-11-12 17:20:48 +0100

    gros lot d'améliorations !

--- 1b_sirene_geo.py
+++ 1b_sirene_geo.py
@@ -42,6 +42,7 @@ header = None
 ok = 0
 total = 0
 numbers = re.compile('(^[0-9]*)')
+stats = {'housenumber':0,'interpolation':0,'street':0,'locality':0,'municipality':0,'vide':0,'townhall':0}
 
 for et in sirene_csv:
     if header is None:
@@ -62,24 +63,27 @@ for et in sirene_csv:
             libvoie = libvoie[len(numvoie):]
 
 
+        # typvoie incorrect
+        if typvoie == 'PRO':
+            typvoie = 'PROM'
+
+        # élimination des LD / LIEU-DIT des libellés
+        if typvoie == 'LD':
+            typvoie = ''
+        libvoie = re.sub(r'^PRO ','PROMENADE ',libvoie)
+        libvoie = re.sub(r'^LD ','',libvoie)
+        libvoie = re.sub(r'^LIEU(.|)DIT ','',libvoie)
+        libvoie = re.sub(r'^ADRESSE INCOMPLETE.*','',libvoie)
+        libvoie = re.sub(r'^SANS DOMICILE FIXE','',libvoie)
+
         # ou de la ligne 4 normalisée
-        ligne4G = '%s%s   %s   %s' % (numvoie, indrep, typvoie, libvoie)
+        ligne4G = '%s%s %s %s' % (numvoie, indrep, typvoie, libvoie)
         ligne4N = et[5]
         ligne4D = et[12]
         # code INSEE de la commune
         depcom = '%s%s' % (et[22],et[23])
 
-        # élimination des LD / LIEU-DIT des libellés
-        if typvoie == 'LD':
-            typvoie = ''
-        if libvoie[:2] == 'LD':
-            libvoie = libvoie[2:]
-        if libvoie[:8] == 'LIEU-DIT':
-            libvoie = libvoie[8:]
-        if libvoie[:8] == 'LIEU DIT':
-            libvoie = libvoie[8:]
-
-        trace(format('%s / %s / %s' % (ligne4G, ligne4D, ligne4N)))
+        trace('%s / %s / %s' % (ligne4G, ligne4D, ligne4N))
 
         # géocodage BAN (ligne4 géo, déclarée ou normalisée si pas trouvé ou score insuffisant)
         ban = geocode(addok_ban, {'q': ligne4G, 'citycode': depcom, 'limit': '1'})
@@ -89,6 +93,7 @@ for et in sirene_csv:
         if ban is None or ban['properties']['score']<score_min and ligne4D != ligne4N:
             ban = geocode(addok_ban, {'q': ligne4D, 'citycode': depcom, 'limit': '1'})
             trace('+ ban  L4D')
+
 
         # géocodage BANO (ligne4 géo, déclarée ou normalisée si pas trouvé ou score insuffisant)
         bano = geocode(addok_bano, {'q': ligne4G, 'citycode': depcom, 'limit': '1'})
@@ -127,34 +132,67 @@ for et in sirene_csv:
         # on a un numéro... on cherche dessus
         if numvoie != '' :
             # numéro trouvé dans les deux bases, on prend BAN sauf si score inférieur de 20% à BANO
-            if ban_type == 'housenumber' and bano_type == 'housenumber' and ban_score > score_min and ban_score >= bano_score*0.8:
+            if ban_type == 'housenumber' and bano_type == 'housenumber' and ban_score > score_min and ban_score >= bano_score/1.2:
                 source = ban
             elif ban_type == 'housenumber' and ban_score > score_min:
                 source = ban
             elif bano_type == 'housenumber' and bano_score > score_min:
                 source = bano
             # on cherche une interpollation dans BAN
-            elif ban_type == 'street' and int(numvoie)>2:
+            elif ban is None or ban_type == 'street' and int(numvoie)>2:
                 ban_avant = geocode(addok_ban, {'q': '%s %s %s' % (int(numvoie)-2, typvoie, libvoie), 'citycode': depcom, 'limit': '1'})
                 ban_apres = geocode(addok_ban, {'q': '%s %s %s' % (int(numvoie)+2, typvoie, libvoie), 'citycode': depcom, 'limit': '1'})
                 if ban_avant is not None and ban_apres is not None:
                     if ban_avant['properties']['type'] == 'housenumber' and ban_apres['properties']['type'] == 'housenumber' and ban_avant['properties']['score']>0.5 and ban_apres['properties']['score']>score_min :
-                        source = ban
+                        source = ban_avant
                         source['geometry']['coordinates'][0] = round((ban_avant['geometry']['coordinates'][0]+ban_apres['geometry']['coordinates'][0])/2,6)
                         source['geometry']['coordinates'][1] = round((ban_avant['geometry']['coordinates'][1]+ban_apres['geometry']['coordinates'][1])/2,6)
                         source['properties']['score'] = (ban_avant['properties']['score']+ban_apres['properties']['score'])/2
                         source['properties']['type'] = 'interpolation'
                         source['properties']['id'] = ''
-                        source['properties']['label'] = numvoie + ' ' + source['properties']['label']
+                        if 'street' in source['properties']:
+                            source['properties']['label'] = '%s %s %s %s' % (numvoie, source['properties']['street'], source['properties']['postcode'], source['properties']['city'])
+                        else:
+                            source['properties']['label'] = '%s %s %s %s' % (numvoie, source['properties']['locality'], source['properties']['postcode'], source['properties']['city'])
+
+        # on essaye sans l'indice de répétition (BIS, TER qui ne correspond pas ou qui manque en base)
+        if source is None and ban is None and indrep != '':
+            trace('supp. indrep BAN : %s %s %s' % (numvoie, typvoie, libvoie))
+            addok = geocode(addok_ban, {'q': '%s %s %s' % (numvoie, typvoie, libvoie), 'citycode': depcom, 'limit': '1'})
+            if addok is not None and addok['properties']['type'] == 'housenumber' and addok['properties']['score'] > score_min:
+                addok['properties']['type'] = 'interpolation'
+                source = addok
+                trace('+ ban  L4G-indrep')
+        if source is None and bano is None and indrep != '':
+            trace('supp. indrep BANO: %s %s %s' % (numvoie, typvoie, libvoie))
+            addok = geocode(addok_bano, {'q': '%s %s %s' % (numvoie, typvoie, libvoie), 'citycode': depcom, 'limit': '1'})
+            if addok is not None and addok['properties']['type'] == 'housenumber' and addok['properties']['score'] > score_min:
+                addok['properties']['type'] = 'interpolation'
+                source = addok
+                trace('+ bano L4G-indrep')
 
         # pas trouvé ? on cherche une rue
         if source is None and typvoie != '':
-            if ban_type == 'street' and bano_type == 'street' and ban_score > score_min and ban_score >= bano_score*0.8:
+            if ban_type == 'street' and bano_type == 'street' and ban_score > score_min and ban_score >= bano_score/1.2:
                 source = ban
             elif ban_type == 'street' and ban_score > score_min:
                 source = ban
             elif bano_type == 'street' and bano_score > score_min:
                 source = bano
+
+        # pas trouvé ? on cherche sans numvoie
+        if source is None and numvoie != '':
+            trace('supp. numvoie : %s %s %s' % (numvoie, typvoie, libvoie))
+            addok = geocode(addok_ban, {'q': '%s %s' % (typvoie, libvoie), 'citycode': depcom, 'limit': '1'})
+            if addok is not None and addok['properties']['type'] == 'street' and addok['properties']['score'] > score_min:
+                source = addok
+                trace('+ ban  L4G-numvoie')
+        if source is None and numvoie != '':
+            addok = geocode(addok_bano, {'q': '%s %s' % (typvoie, libvoie), 'citycode': depcom, 'limit': '1'})
+            if addok is not None and addok['properties']['type'] == 'street' and addok['properties']['score'] > score_min:
+                source = addok
+                trace('+ bano L4G-numvoie')
+
 
         # pas trouvé ? tout type accepté...
         if source is None:
@@ -170,10 +208,23 @@ for et in sirene_csv:
             try:
                 i = commune_insee.index(depcom)
                 sirene_geo.writerow(et+[commune_longitude[i],commune_latitude[i],0,'municipality','',commune_insee[i]])
+                if ligne4G.strip() !='':
+                    print('(%s) %s' % (depcom, ligne4G.strip()))
+                    if typvoie == '' and (libvoie == 'MAIRIE' or libvoie == 'HOTEL DE VILLE'):
+                        stats['townhall']+=1
+                        ok = ok +1
+                    else:
+                        stats['municipality']+=1
+                else:
+                    stats['vide']+=1
+                    ok = ok +1
             except:
                 sirene_geo.writerow(et+['','',0,'','',''])
         else:
             ok = ok +1
+            if ['village','town','city'].count(source['properties']['type'])>0:
+                source['properties']['type'] = 'municipality'
+            stats[source['properties']['type']]+=1
             sirene_geo.writerow(et+[source['geometry']['coordinates'][0],
                                     source['geometry']['coordinates'][1],
                                     round(source['properties']['score'],2),
@@ -181,4 +232,6 @@ for et in sirene_csv:
                                     source['properties']['label'],
                                     source['properties']['id']])
         if total % 1000 == 0:
-            print(total, round(100*ok/total,2))
+            print(total, round(100*ok/total,2), stats)
+
+print(stats)
