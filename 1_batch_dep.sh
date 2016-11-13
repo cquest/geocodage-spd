8c175cddcd9426fbaaf1b21059bb8568bab61559
cquest <cquest@openstreetmap.fr>
2016-11-13 10:54:54 +0100

    séparation traitements + ordre décroissant de volume

--- 1_batch_dep.sh
+++ 1_batch_dep.sh
@@ -3,5 +3,9 @@
 mkdir -p sirene
 mkdir -p sirene_geo
 
+# découpage du fichier national en fichiers par département (DOM groupés)
 echo {01..19} 2A 2B {21..98} | sed 's/ /\n/g' | \
-  parallel -j 24 -t csvgrep sirene_mini.csv -c DEPET -m {} \> sirene_{}.csv \; ./1b_sirene_geo.py sirene_{}.csv \> sirene_{}.csv.log
+  parallel -j 24 -t csvgrep sirene.csv -c DEPET -m {} \> sirene_{}.csv
+# géocodage par taille décroissante...
+wc -l sirene_*.csv | sort -n -r | grep ...csv -o | sed 's/.csv//' | \
+  parallel -j 24 -t  ./1b_sirene_geo.py sirene_{}.csv \> sirene_{}.csv.log
