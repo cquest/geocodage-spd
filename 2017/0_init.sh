523ff7f23809919d809c72b3ca436ea07b529a4f
cquest <cquest@openstreetmap.fr>
2017-02-03 08:37:35 +0100

    découpage Paris et DOM

--- 2017/0_init.sh
+++ 2017/0_init.sh
@@ -14,6 +14,18 @@ mkdir -p sirene_geo
 # découpage du fichier national en fichiers par département (DOM groupés)
 echo {01..19} 2A 2B {21..98} | sed 's/ /\n/g' | \
   parallel -j 24 -t csvgrep sirene-mini.csv -c DEPET -m {} \> sirene_{}.csv
+
+# découpage Paris
+echo {101..120} | sed 's/ /\n/g' | \
+    parallel -j 24 -t csvgrep sirene_75.csv -c COMET -m {} \> sirene_75{}.csv
+
+# découpage DOM
+echo {1..6} | sed 's/ /\n/g' | \
+    parallel -j 24 -t csvgrep sirene_97.csv -c COMET -r "{}.*" \> sirene_97{}.csv
+
+rm sirene_97.csv
+rm sirene_75.csv
+
 # géocodage par taille décroissante...
-wc -l sirene_*.csv | sort -n -r | grep ...csv -o | sed 's/.csv//' | \
-  parallel -j 24 -t  ./1b_sirene_geo.py sirene_{}.csv \> sirene_{}.csv.log
+wc -l sirene_*.csv | sort -n -r | grep 'sirene_.*.csv' -o | \
+  parallel -j 24 -t  ./1b_sirene_geo.py {} \> {}.log
