6d5e3334ee83070a4bc421abf4e25412ba0d3410
cquest <cquest@openstreetmap.fr>
2017-03-02 09:36:30 +0100

    prise en compte double quotes

--- 2017/2_split_rsync.sh
+++ 2017/2_split_rsync.sh
@@ -5,9 +5,8 @@ mkdir -p communes
 
 # entête des fichiers CSV par commune
 for i in `csvcut -c 23,24 $1 | sort -u | sed 's/,//' | grep '^[0-9]'`; do
-  head -n 1 geo-sirene_92.csv > 2017-01/communes/$i.csv;
+  head -n 1 geo-sirene_01.csv > communes/$i.csv;
 done
 
 # split des fichiers
-tail -n +2 $1 | awk -v FPAT='[^,]*|"[^"]*"' '{print >> "communes/"$23$24".csv"}' 
-
+tail -n +2 $1 | awk -v FPAT='[^,]*|"([^"]|"")*"' '{print >> "communes/"$23$24".csv"}'
