8b9c84036963fb936a3d1e0115fd524a3651afa7
cquest <cquest@openstreetmap.fr>
2018-06-21 09:16:49 +0200

    conservation des libellés INSEE

--- insee-sirene/2_split_rsync.sh
+++ insee-sirene/2_split_rsync.sh
@@ -4,9 +4,9 @@ echo $1
 mkdir -p communes
 
 # entête des fichiers CSV par commune
-for i in `csvcut -c 23,24 $1 | sort -u | sed 's/,//' | grep '^[0-9]'`; do
+for i in `csvcut -c 25,28 $1 | sort -u | sed 's/,//' | grep '^[0-9]'`; do
   head -n 1 geo-sirene_01.csv > communes/$i.csv;
 done
 
 # split des fichiers
-tail -n +2 $1 | awk -v FPAT='[^,]*|"([^"]|"")*"' '{print >> "communes/"$23$24".csv"}'
+tail -n +2 $1 | awk -v FPAT='[^,]*|"([^"]|"")*"' '{print >> "communes/"$25$28".csv"}'
