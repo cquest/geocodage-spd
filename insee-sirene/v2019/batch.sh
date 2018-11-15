0e771d5863fb210a6170d604848dcc450e1b0e7e
cquest <cquest@openstreetmap.fr>
2018-11-15 18:21:10 +0100

    adaption pour les changement apparus avec le stock de novembre 2018

--- insee-sirene/v2019/batch.sh
+++ insee-sirene/v2019/batch.sh
@@ -1,22 +1,21 @@
+#! /usr/bash
+
 mkdir -p data
 
 echo "1/5 : téléchargement"
 # récupération des URL des fichiers
 curl 'https://www.data.gouv.fr/api/1/datasets/5b7ffc618b4c4169d30727e0/' -s \
   | jq .resources[].url -r \
-  | grep stocketablissement-utf8.zip \
-  | sort | tail -n 1> urls.txt
+  | grep -i StockEtablissement_utf8.zip \
+  | sort | tail -n 1 > urls.txt
 wget -nc -i urls.txt
 
-echo "1b/5 : décompression"
-unzip stocketablissement-utf8.zip
-
 echo "2/5 : découpage en fichiers départementaux"
-awk -v FPAT='[^,]*|"([^"]|"")*"' '{ print >> "data/dep_"substr($21,1,2)".csv"}' StockEtablissement_utf8.csv
+rm -f data/dep_*.csv
+unzip -p StockEtablissement_utf8.zip | awk -v FPAT='[^,]*|"([^"]|"")*"' '{ print >> "data/dep_"substr($21,1,2)".csv"}'
 awk -v FPAT='[^,]*|"([^"]|"")*"' '{ print >> "data/dep_"$21".csv"}' data/dep_75.csv
 awk -v FPAT='[^,]*|"([^"]|"")*"' '{ print >> "data/dep_"substr($21,1,3)".csv"}' data/dep_97.csv
-rm data/dep_75.csv data/dep_97.csv data/dep_co.csv
-rm stocketablissement-utf8.csv
+rm -f data/dep_75.csv data/dep_97.csv data/dep_co.csv
 
 cd ..
 echo "3/5 : géocodage par taille décroissante"
@@ -36,11 +35,14 @@ rm communes/.csv
 cd -
 
 echo "5/5 : fusion fichier national"
-head -n 1 data/geo_dep_01.csv | gzip -9 > StockEtablissement_utf8_geo.csv.gz
-for f in data/geo_dep*.csv.gz; do zcat "$f" | tail -n +2 | gzip -9 >> StockEtablissement_utf8_geo.csv.gz ; done
+zcat data/geo_siret_01.csv.gz | head -n 1 | gzip -9 --rsyncable > StockEtablissement_utf8_geo.csv.gz
+for f in data/geo_siret*.csv.gz; do zcat "$f" | tail -n +2 | gzip -9 --rsyncable >> StockEtablissement_utf8_geo.csv.gz ; done
 
-# fichier séparé pour établissements actifs seuls
-zcat StockEtablissement_utf8_geo.csv.gz | csvgrep -c etatadministratifetablissement -m "A" | gzip -9 > StockEtablissementActif_utf8_geo.csv.gz
+# fichier séparé pour établissements actifs/fermés seuls
+zcat StockEtablissement_utf8_geo.csv.gz \
+| tee >(csvgrep -c etatAdministratifEtablissement -m "A" | gzip -9 --rsyncable > StockEtablissementActif_utf8_geo.csv.gz) \
+      >(csvgrep -c etatAdministratifEtablissement -m "F" | gzip -9 --rsyncable > StockEtablissementFerme_utf8_geo.csv.gz) \
+      > /dev/null
 
 rsync *.gz root@data.cquest.org:/var/www/html/data/geo_sirene/v2019/AAAA-MM -av --progress
 rsync data/geo_*.gz root@data.cquest.org:/var/www/html/data/geo_sirene/v2019/AAAA-MM -av --progress
@@ -53,7 +55,7 @@ rsync data/logs.tgz root@data.cquest.org:/var/www/html/data/geo_sirene/v2019/AAA
 rsync data/communes/ root@data.cquest.org:/var/www/html/data/geo_sirene/v2019/AAAA-MM/communes -avz --progress
 
 mv data/geo_*.gz "AAAA-MM" # fichiers départementaux + logs
-mv communes "AAAA-MM" # fichiers communaux
+mv data/communes "AAAA-MM" # fichiers communaux
 mv *.gz "AAAA-MM"
 
 # extraction liste des CEDEX, libelles et code INSEE (DEP/COM)
